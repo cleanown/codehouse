d26b77fb92161b75541b445a00a5b54044ff6a31
cleanown <1346387492@qq.com>
2020-04-15 20:20:29 +0800

    1

---
+++ src/components/user/usermg.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="home">
+    <div class="headerArea">
+      <v-text-field
+        label="输入用户的相关信息"
+        prepend-inner-icon="mdi-cloud-search-outline"
+        solo
+        v-model="searchvalue"
+        clearable
+        hide-details
+        icon
+      >
+      <template v-slot:append>
+        <v-btn
+          icon
+          @click="handleSearchClick"
+        >
+          <v-icon>mdi-table-search</v-icon>
+        </v-btn>
+      </template>
+      </v-text-field>
+    </div>
+
+    <v-list
+      class="searchlist"
+      v-show="!wait"
+      >
+      <v-list-item
+        v-for="(item, index) in userlist"
+        :key="index"
+        class="list"
+        style="border-bottom: 1px solid #eee"
+      >
+        <v-list-item-icon>
+          <v-img src="@/assets/img/1.jpg"></v-img>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="item.username"></v-list-item-title>
+        </v-list-item-content>
+
+        <v-list-item-avatar size="80" height="40">
+          <v-list-item-title v-text="item.role"></v-list-item-title>
+        </v-list-item-avatar>
+      </v-list-item>
+    </v-list>
+
+    <div class="wait">
+      <v-progress-circular
+        class="wait-progress"
+        :size="100"
+        :width="3"
+        indeterminate
+        color="deep-purple"
+        v-show="wait"
+      >
+        加载中...
+      </v-progress-circular>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'usermg',
+  data () {
+    return {
+      searchvalue: '',
+      wait: false,
+      userlist: [{
+        username: 'claennasdf',
+        role: 1
+      }, {
+        username: 'aaaa',
+        role: 1
+      }, {
+        username: 'dddd',
+        role: 2
+      }, {
+        username: 'cffff',
+        role: 2
+      }, {
+        username: 'ccccf',
+        role: 1
+      }]
+    }
+  },
+  mounted () {
+    this.userAdmin()
+  },
+  methods: {
+    userAdmin () {
+      if (this.userlist.role === 1) {
+        this.userlist.role = '普通用户'
+        console.log(this.userlist.role)
+      } else {
+        this.userlist.role = '超级管理员'
+      }
+    },
+    handleSearchClick () {
+      console.log('111')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home{
+  margin-top: 56px;
+  padding: 10px;
+  .headerArea {
+    margin-top: 56px;
+    background: #fff;
+    position: fixed;
+    z-index: 2;
+    top: 0;
+    left: 0;
+    height: 68px;
+    width: 100%;
+    padding: 10px 20px;
+  }
+  .searchlist{
+    margin-top: 56px;
+  }
+}
+</style>
--- src/views/UserManage.vue
+++ src/views/UserManage.vue
@@ -11,102 +11,16 @@
       <v-icon @click.stop="drawer = !drawer">mdi-sort</v-icon>
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-      bottom
-    >
-      <p class="drawer-title">筛选</p>
-      <div class="drawer-switch">
-        <v-switch v-model="isverify" class="mx-2" label="待审核"></v-switch>
-        <v-switch v-model="isdelete" class="mx-2" label="已删除"></v-switch>
-      </div>
-      <div class="drawer-time">
-        <div>发布时间 :</div>
-        <div class="d-flex drawer-time-select">
-          <v-menu
-            ref="beginmenu"
-            v-model="beginmenu"
-            :close-on-content-click="false"
-            :return-value.sync="begindate"
-            transition="scale-transition"
-            offset-y
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on }">
-              <v-text-field
-                v-model="begindate"
-                label="开始日期"
-                prepend-icon="mdi-calendar"
-                readonly
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="begindate"
-              no-title scrollable
-              min="2020-03-01"
-              :max="date"
-            >
-              <v-spacer></v-spacer>
-              <v-btn text color="primary" @click="beginmenu = false">取消</v-btn>
-              <v-btn text color="primary" @click="$refs.beginmenu.save(begindate)">确认</v-btn>
-            </v-date-picker>
-          </v-menu>
-          <v-spacer></v-spacer>
-          <v-menu
-            ref="endmenu"
-            v-model="endmenu"
-            :close-on-content-click="false"
-            :return-value.sync="enddate"
-            transition="scale-transition"
-            offset-y
-            min-width="290px"
-          >
-            <template v-slot:activator="{ on }">
-              <v-text-field
-                v-model="enddate"
-                label="结束日期"
-                prepend-icon="mdi-calendar"
-                readonly
-                v-on="on"
-              ></v-text-field>
-            </template>
-            <v-date-picker
-              v-model="enddate"
-              no-title scrollable
-              min="2020-03-01"
-              :max="date"
-            >
-              <v-spacer></v-spacer>
-              <v-btn text color="primary" @click="endmenu = false">取消</v-btn>
-              <v-btn text color="primary" @click="$refs.endmenu.save(enddate)">确认</v-btn>
-            </v-date-picker>
-          </v-menu>
-        </div>
-      </div>
-      <div class="drawer-btn">
-        <v-btn
-          width="100%"
-          color="primary"
-          @click="handleSelectClick"
-        >
-          确认
-        </v-btn>
-      </div>
-    </v-navigation-drawer>
-
-    <admin-mg class="detail" @changeValue="changeValue" />
+    <user-mg />
 
     <div class="footer">
       <v-pagination
         v-model="page"
         :length="pagetotal"
       ></v-pagination>
-  </div>
+    </div>
 
-  <v-snackbar
+    <v-snackbar
       v-model="snackbar"
       top
       :timeout="timeout"
@@ -125,39 +39,21 @@
 </template>
 
 <script>
-import config from '../request/config'
-import AdminMg from '../components/manage/articlemg'
+import UserMg from '../components/user/usermg'
 export default {
   name: 'useremanage',
   data () {
     return {
-      drawer: null,
-      items: [
-        { title: 'Home', icon: 'dashboard' },
-        { title: 'About', icon: 'question_answer' }
-      ],
       page: 1,
       pagetotal: 1,
-      isverify: false,
-      isdelete: false,
       snackbar: false,
       text: '',
       timeout: 2000,
-      searchValue: '',
-      date: new Date().toISOString().substr(0, 10),
-      begindate: new Date().toISOString().substr(0, 10),
-      beginmenu: false,
-      enddate: new Date().toISOString().substr(0, 10),
-      endmenu: false,
-      beginTime: '',
-      endtime: ''
+      searchValue: ''
     }
   },
   components: {
-    AdminMg
-  },
-  mounted () {
-    this.apidataGet()
+    UserMg
   },
   methods: {
     handleBackClick () {
@@ -166,39 +62,6 @@ export default {
     changeValue (val) {
       this.searchValue = val
       this.page = 1
-      this.apidataGet()
-    },
-    handleSelectClick () {
-      this.beginTime = (this.begindate.slice(5, 7)) * 2592000000 + this.begindate.slice(8, 10) * 86400000
-      this.endTime = (this.enddate.slice(5, 7)) * 2592000000 + this.enddate.slice(8, 10) * 86400000
-      if (this.beginTime > this.endTime) {
-        this.snackbar = true
-        this.text = '开始时间应小于结束时间'
-      } else {
-        this.apidataGet()
-        this.drawer = false
-      }
-    },
-    async apidataGet () {
-      const url = `${config.online}/search/adminCompanyList`
-      const data = {
-        page: this.page,
-        isverify: this.isverify,
-        isdelete: this.isdelete,
-        hotkey: this.searchValue,
-        beginTime: this.beginTime,
-        endTime: this.endTime
-      }
-      const res = await this.$http.post(url, data)
-      if (res.data.code === 200) {
-        this.$store.commit('adminmg', (res.data.data))
-        console.log(res.data.data)
-        this.page = res.data.data.page
-        this.pagetotal = res.data.data.pageTotle
-      } else {
-        this.snackbar = true
-        this.text = res.msg
-      }
     }
   }
 }
@@ -210,29 +73,6 @@ export default {
 .header{
   position: fixed;
   z-index: 3;
-}
-.drawer-title{
-  text-align: center;
-  font-size: 20px;
-  font-weight: bolder;
-  margin: 10px;;
-}
-.drawer-switch{
-  display: flex;
-  justify-content: space-around;
-}
-.drawer-time{
-  padding: 0 20px;
-  .drawer-time-select{
-    margin-top: 20px;
-  }
-}
-.drawer-btn{
-  position: fixed;
-  width: 100%;
-  padding: 0 50px;
-  margin-bottom: 40px;
-  bottom: 0;
 }
 .footer{
   position: fixed;
